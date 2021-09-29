4f5c12697af61f3fa18d40aa59bac96aea09271c
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-09-29 20:44:44 +0600

    create component , using material ui grid & card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button, Rating } from '@mui/material';
 function App() {
   return (
     <div className="App">
-      
+      <Button variant="contained" >Helo </Button>
+
+     
+ <Rating name="read-only" value={5} readOnly />
     </div>
   );
 }
